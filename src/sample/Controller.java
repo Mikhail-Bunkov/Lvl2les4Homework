d2dd68bfb012a05e2ce2d75db841981d75f4f841
Mikhail-Bunkov <bunko04@bk.ru>
2021-02-17 15:38:40 +1000

    Задание выполнено, только я не сделал так, чтобы textarea нельзя было выбрать в фокус

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 public class Controller {
+    @FXML
+    public TextArea textArea;
+    @FXML
+    public TextField textField;
+
+    @FXML
+    public void ClickSendBtn(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
